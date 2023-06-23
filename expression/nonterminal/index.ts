0148a9973ab81111316b625b638d608d796794e1
Quirax Lee <qrlee@devguru.co.kr>
2023-06-23 21:11:58 +0900

    Created operators to access stdin and stdout

--- expression/nonterminal/index.ts
+++ expression/nonterminal/index.ts
@@ -1,10 +1,13 @@
 import { Decrease, Increase } from './accMutator.ts'
 import { AppointedMemLoad, AppointedMemSave } from './appointedMem.ts'
 import Operator from './operator.ts'
+import { StandardInput, StandardOutput } from './stdio.ts'
 
 export default function registerOps() {
     Operator.register(Increase)
     Operator.register(Decrease)
     Operator.register(AppointedMemLoad)
     Operator.register(AppointedMemSave)
+    Operator.register(StandardInput)
+    Operator.register(StandardOutput)
 }
