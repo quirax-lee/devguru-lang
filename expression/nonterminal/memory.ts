200917965c2a4511b40a86c100f46caabd6d667f
Quirax Lee <qrlee@devguru.co.kr>
2023-07-02 12:14:59 +0900

    Added operators

--- expression/nonterminal/memory.ts
+++ expression/nonterminal/memory.ts
@@ -31,3 +31,65 @@ export class AppointedMemLoad extends Operator {
         return result
     }
 }
+
+export class LoadPointer extends Operator {
+    public static regexp: string = 'reu'
+
+    public run(): number {
+        let acc = this.getMachine().getAccumulator()
+        let ptr = this.getMachine().getPointer()
+
+        let result = ptr.get()
+
+        acc.set(result)
+
+        return result
+    }
+}
+
+export class SetPointer extends Operator {
+    public static regexp: string = 'rgu'
+
+    public run(): number {
+        let acc = this.getMachine().getAccumulator()
+        let ptr = this.getMachine().getPointer()
+
+        let result = Math.min(Math.max(acc.get(), 1), 255) // result의 범위를 1~255로 고정
+
+        ptr.set(result)
+
+        return result
+    }
+}
+
+export class LoadPointeredMemory extends Operator {
+    public static regexp: string = 'reuu'
+
+    public run(): number {
+        let acc = this.getMachine().getAccumulator()
+        let ptr = this.getMachine().getPointer()
+        let mem = this.getMachine().getMemory(ptr.get())
+
+        let result = mem.get()
+
+        acc.set(result)
+
+        return result
+    }
+}
+
+export class SavePointeredMemory extends Operator {
+    public static regexp: string = 'rguu'
+
+    public run(): number {
+        let acc = this.getMachine().getAccumulator()
+        let ptr = this.getMachine().getPointer()
+        let mem = this.getMachine().getMemory(ptr.get())
+
+        let result = acc.get()
+
+        mem.set(result)
+
+        return result
+    }
+}
