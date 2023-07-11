5f91f25ee79548ca7361762d71cd268440897fc0
Quirax Lee <qrlee@devguru.co.kr>
2023-07-11 21:43:15 +0900

    Tested conditional statements

--- expression/nonterminal/jumper.test.ts
+++ expression/nonterminal/jumper.test.ts
@@ -1,1 +1,20 @@
 import test from '../../common/tester.ts'
+
+test([
+    {
+        script: 'v eru d egu gru',
+        run(machine) {
+            it('M1 == 1', () => {
+                expect(machine.getMemory(1).get()).toBe(1)
+            })
+        },
+    },
+    {
+        script: 'd eruu d eguu gruu',
+        run(machine) {
+            it('M2 == -1', () => {
+                expect(machine.getMemory(2).get()).toBe(-1)
+            })
+        },
+    },
+])
