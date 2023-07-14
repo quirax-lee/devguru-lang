78b62f854d7bf3e036daff3fa4a550601d8e840b
Quirax Lee <qrlee@devguru.co.kr>
2023-07-14 19:17:24 +0900

    Added test cases for conditional loop

--- expression/nonterminal/jumper.test.ts
+++ expression/nonterminal/jumper.test.ts
@@ -18,3 +18,42 @@ test([
         },
     },
 ])
+
+test([
+    {
+        script: 'vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv',
+        run(machine) {
+            it('Acc == 76', () => {
+                expect(machine.getAccumulator().get()).toBe(76)
+            })
+        },
+    },
+    {
+        script: 'egu d eru',
+        run(machine) {
+            it('Acc == 0', () => {
+                expect(machine.getAccumulator().get()).toBe(0)
+            })
+        },
+    },
+])
+
+test([
+    {
+        script: 'vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd',
+        run(machine) {
+            it('Acc == -26', () => {
+                expect(machine.getAccumulator().get()).toBe(-26)
+                // expect(machine.getAccumulator().get()).toBe(76)
+            })
+        },
+    },
+    {
+        script: 'gru gruu egu geu v gru eruu v eruuu eguu geuu d gruu eru v eguuu d',
+        run(machine) {
+            it('Acc == 0', () => {
+                expect(machine.getAccumulator().get()).toBe(0)
+            })
+        },
+    },
+])
